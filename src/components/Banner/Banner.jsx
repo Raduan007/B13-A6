0e31d4dd20de99cdaa7627b96b6b143f8fb3acf0
raduan007 <raduanhossen517@gmail.com>
2026-04-01 16:16:11 +0600

    font-family added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,13 +4,13 @@ import bannerImg from '../../assets/banner.png'
 const Banner = () => {
     return (
         <div className='mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center mt-10 max-w-7xl px-2 text-center lg:text-left'>
-            <div className="space-y-4"> 
+            <div className="space-y-4 pl-20"> 
                 <div className="inline-flex items-center bg-[#E1E7FF] px-3 py-2 rounded-full gap-2 mx-auto lg:mx-0">
                     <img src="/Group5.png" alt="icon" className="w-4 h-4 object-contain" />
                     <span className='bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent'>New: AI-Powered Tools Available</span>
                 </div>
                 <h1 className='text-4xl md:text-5xl lg:text-6xl text-[#101727] font-semibold py-2'>Supercharge Your Digital Workflow</h1>
-                <p className='opacity-70 pb-4'>Access premium AI tools, design assets, templates, and productivity software—all in one place. Start creating faster today.Explore Products</p> 
+                <p className='opacity-70 pb-4'>Access premium AI tools, design assets, templates, and productivity <br /> software—all in one place. Start creating faster today. <br /> Explore Products</p> 
                 <div className='flex flex-col sm:flex-row gap-5 items-center justify-center lg:justify-start'>
                     <button className="bg-gradient-to-r from-[#4F39F6] to-[#9514FA] px-4 py-2 rounded-[50px] text-white">Explore Products</button>
                     <div className="p-[2px] bg-gradient-to-r from-[#4F39F6] to-[#9514FA] rounded-[50px] ">
